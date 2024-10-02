d4df93fffa01b76502328cdc2b547a7e71f7a474
BreweryIS <moogle.fanfare@gmail.com>
2024-10-02 17:08:52 +0700

    done

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import React, { createContext, useState } from "react";
+import Menu from "./components/Menu";
+import Quiz from "./components/Quiz";
+import Score from "./components/Score";
+
+export const DataContext = createContext();
+
+const App = () => {
+  const [appState, setAppState] = useState("menu");
+  const [score, setScore] = useState(0);
+
+  return (
+    <DataContext.Provider value={{ appState, setAppState, score, setScore }}>
+      <div className="App">
+        <h1>Web Development Quiz</h1>
+        {appState === "menu" && <Menu />}
+        {appState === "quiz" && <Quiz />}
+        {appState === "score" && <Score />}
+      </div>
+    </DataContext.Provider>
+  );
+};
+
+export default App;
